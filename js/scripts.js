587c41cdaeb202399e31ad42cdd3cd1cfc823e23
Viren Bhosale <virubhosale112@gmail.com>
2020-08-14 14:45:00 +0530

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+function copyFn() {
+    if (document.getElementById('code') == null)
+        return false;
+    else {
+        var range = document.createRange();
+        range.selectNode(document.getElementById("code"));
+        window.getSelection().removeAllRanges(); // clear current selection
+        window.getSelection().addRange(range); // to select text
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges(); // to deselect
+        alert('copied!');
+    }
+}
+var r, g, b, clr;
+
+function changeClr() {
+    r = $('#redClr').val();
+    g = $('#greenClr').val();
+    b = $('#blueClr').val();
+    if (r < 127 && g < 127 || g < 127 && b < 127 || r < 127 && b < 127) {
+        $('body').css('color', 'white');
+    } else {
+        $('body').css('color', 'black');
+
+    }
+    $('#redClrData').html(r);
+    $('#greenClrData').html(g);
+    $('#blueClrData').html(b);
+    clrRGB = 'rgb(' + r + ',' + g + ',' + b + ')';
+    $('body').css('background-color', clrRGB);
+    $('#clrCodeSpanRGB').html(clrRGB);
+    r = parseInt(r);
+    g = parseInt(g);
+    b = parseInt(b);
+    clrHEX = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    $('#clrCodeSpanHEX').html(clrHEX);
+
+}
